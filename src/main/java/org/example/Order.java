aac7d93946ace73153ff670ff34d5a5ac2f4185d
Elena Prokopeva <g777elenagromova@yandex.ru>
2023-02-26 10:18:42 +0600

    2023.02.26 диплом2 Прокопьева версия2 - исправила замечания - исправила неработающие тесты - добавила создание заказа через ингредиенты из БД

--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -1,21 +1,13 @@
 package org.example;
 
+import java.util.List;
+
 public class Order {
 
-    private String[] ingredients;
+    private List<String> ingredients;
 
-    public Order(String[] ingredients){
+    public Order(List<String> ingredients){
         this.ingredients = ingredients;
     }
 
-    public Order() {
-    }
-
-    public String[] getIngredients() {
-        return ingredients;
-    }
-
-    public void setIngredients(String[] ingredients) {
-        this.ingredients = ingredients;
-    }
 }
--- src/test/java/UserOrdersListTest.java
+++ src/test/java/UserOrdersListTest.java
@@ -1,4 +1,5 @@
 import api.BaseApi;
+import api.IngredientApi;
 import api.OrderApi;
 import api.UserApi;
 import io.qameta.allure.Description;
@@ -8,23 +9,39 @@ import org.example.User;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
+
 import static org.apache.http.HttpStatus.*;
 import static org.hamcrest.Matchers.*;
 
 public class UserOrdersListTest extends BaseApi {
 
+    private final static String USER_LOGIN = "g77750elenagromova@yandex.com";
+    private final static String USER_PASSWORD = "1234";
+    private final static String USER_NAME = "Elena";
+
     private OrderApi orderApi;
     private UserApi userApi;
+    private String token;
+    private Order order;
+    private IngredientApi ingredientApi;
+    private List<String> ingredients;
 
     @Before
     public void setUp() {
         super.setupRequestSpecification();
         this.orderApi = new OrderApi(requestSpecification);
         this.userApi = new UserApi(requestSpecification);
-        userApi.setUser(new User("g77750elenagromova@yandex.com", "1234", "Elena"));
+        this.ingredientApi = new IngredientApi(requestSpecification);
+        userApi.setUser(new User(USER_LOGIN, USER_PASSWORD, USER_NAME));
         userApi.createUser().then().statusCode(SC_OK);
-       for (int i = 0; i < 3; i++){
-            orderApi.addOrder(new Order(new String[]{"60d3463f7034a000269f45e9",  "60d3463f7034a000269f45e7"}));
+        token = userApi.getTokenUser();
+        ingredients = ingredientApi.getIngredientsList();
+        order = new Order(List.of(ingredients.get(0), ingredients.get(1)));
+        for (int i = 0; i < 3; i++){
+           orderApi.setOrder(order);
+           orderApi.createOrder(token);
         }
 
     }
@@ -35,7 +52,7 @@ public class UserOrdersListTest extends BaseApi {
     @DisplayName("Проверка получения списка заказов (пользователь авторизован)")
     @Description("Успешная проверка создания заказа (пользователь авторизован)")
     public void checkGetOrdersListWithUserAuth(){
-        orderApi.getUserOrdersList(userApi.getTokenUser())
+        orderApi.getUserOrdersList(token)
                 .then().assertThat().body("success", is(true))
                 .assertThat().body("total", notNullValue())
                 .assertThat().body("orders", notNullValue())
@@ -49,19 +66,16 @@ public class UserOrdersListTest extends BaseApi {
     @DisplayName("Проверка получения списка заказов (пользователь не авторизован)")
     @Description("Успешная проверка создания заказа (пользователь не авторизован)")
     public void checkGetOrdersListWithoutUserAuth(){
-        orderApi.getUserOrdersList(null)
+        orderApi.getUserOrdersList("null")
                 .then().assertThat().body("success", is(false))
-                .assertThat().body("message", equalTo("You should be authorised"))
+                .assertThat().body("message", equalTo("jwt malformed"))
                 .and()
-                .statusCode(SC_UNAUTHORIZED);
+                .statusCode(SC_FORBIDDEN);
     }
 
     @After
     public void dataClean(){
-        userApi.deleteUser();
-//        for (Order order: orderApi.getOrders()) {
-//            orderApi.deleteOrder();
-//        }
+        userApi.deleteUser(userApi.getTokenUser());
     }
 
 }
